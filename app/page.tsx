2d17d4e1971e9fb41c5bb2f830a6719009cc2899
P Nikita <92731180+nikita8763@users.noreply.github.com>
2024-01-25 21:13:22 +0530

    updated page, added showmore

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,60 @@
 "use client"
 
-import { Hero, SearchBar, CustomFilter, CarCard } from '@/components'
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 
-export default async function Home() {
-  const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || '',
-    year: searchParams.year || 2022,
-    fuel: searchParams.fuel || '',
-    limit: searchParams.limit || 10,
-    model: searchParams.model || '',
-  });
-  
+export default function Home() {
+  // const allCars = await fetchCars({
+  //   manufacturer: searchParams.manufacturer || "",
+  //   year: searchParams.year || 2022,
+  //   fuel: searchParams.fuel || "",
+  //   limit: searchParams.limit || 10,
+  //   model: searchParams.model || "",
+
+
+  // converting all this to client side rendering to fetch data
+
+  const [allCars, setAllCars] = useState([]); 
+  const [loading, setLoading] = useState(false);
+
+  // search states
+  const [manufacturer, setManufacturer] = useState(""); 
+  const [model, setModel] = useState("");
+
+  // filter states
+  const [fuel, setFuel] = useState("");
+  const [year, setYear] = useState(2022);
+
+  // pagination states
+  const [limit, setLimit] = useState(10);
+
+  const getCars = async() => {
+    setLoading(true);
+
+    try{
+    const result = await fetchCars({
+        manufacturer: manufacturer || "",
+        year: year || 2022,
+        fuel: fuel || "",
+        limit: limit || 10,
+        model: model || "",
+    });
+
+    setAllCars(result);
+  } catch(error) {
+      console.log(error);
+  } finally {
+      setLoading(false);
+  }
+}
+
+  useEffect(() => {
+    getCars();
+  },[fuel,year,limit,manufacturer,model])
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length<1 || !allCars;
 
   return (
@@ -25,20 +67,39 @@ export default async function Home() {
           </div>
 
           <div className='home__filters'>
-              <SearchBar/>
+              <SearchBar setManufacturer={setManufacturer}
+                          setModel={setModel}         
+              />
               <div className='home__filter-container'>
-                <CustomFilter title="fuel"/>
-                <CustomFilter title="year"/>
+                <CustomFilter title="fuel" options={fuels} setFilter={setFuel}/>
+                <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear}/>
               </div>
           </div>
 
-          {!isDataEmpty ? (
+          {allCars.length > 0 ? (
             <section>
               <div className='home__cars-wrapper'>
                   {allCars?.map((car) => (
                       <CarCard car={car}/>
                   ))}
               </div>
+
+              {loading && (
+                <div className="mt-16 w-full flex-center">
+                  <Image
+                    src="/loading.svg"
+                    alt='loader'
+                    width={50}
+                    height={50}
+                    className='object-contain'
+                  />
+                </div>
+              )}
+
+              <ShowMore
+                pageNumber={(searchParams.limit || 10)/10} //current page number
+                isNext={(searchParams.limit || 10) > allCars.length}
+              />
             </section>
           ): (
             <div className='home__error-container'>
@@ -46,6 +107,8 @@ export default async function Home() {
               <p>{allCars?.message}</p>
             </div>
           )}
+
+
         </div>
     </main>
   )
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,14 @@
-// import { CarProps } from "@/types";
+// import { CarProps, FilterProps } from "@/types";
 
 // export async function fetchCars(filters: FilterProps ) {
+//     const { manufacturer, year, model, limit, fuel } = filters;
 //     const headers= {
 //         'X-RapidAPI-Key': '596bd8b777mshf4e1e1c4b49af8bp13275djsn10851e31404b',
 //         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 //     }
 
-//     const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',{
+//     const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+//     {
 //         headers: headers,
 //     });
 
@@ -43,3 +45,12 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   
     return `${url}`;
 } 
+
+export const updateSearchParams = (type: string, value:string) => {
+    const searchParams = new URLSearchParams(window.location.search); //current search parameters
+
+      searchParams.set(type, value)
+        
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+      return newPathname;
+}
